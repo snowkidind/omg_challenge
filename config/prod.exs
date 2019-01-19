cd07956ba67d5732bac4bd64facb0e224b01d53c
keny <keny@eastcoastbands.com>
2019-01-19 14:09:46 -0500

    SSL port issue

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 #  cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :omg, OmgWeb.Endpoint,
-  url: [host: "https://warm-ravine-32081.herokuapp.com/", port: 443],
+  url: [host: "https://warm-ravine-32081.herokuapp.com/", port: System.get_env("PORT") || 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
